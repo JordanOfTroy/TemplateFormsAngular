6a19910b9964ca27e99cabf028bce3f6ae39a39d
Jordan <jordan@Jordans-MacBook-Air.local>
2023-08-28 09:51:23 -0600

    created an employee class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'day10TemplateDrivenForms';
+
+  emp =  new Employee(
+    'Henry ',
+    'Cavill',
+    true,
+    'W2'
+    );
+
+  ngOnInit(): void {
+    console.log('Employee\'s name is ' + this.emp.firstName + this.emp.lastName)
+  }
 }
